7527fe861ce646458f2828380f53e75c7aa1ae8b
Daniloom <trump.danielhenri@gmail.com>
2026-04-15 14:58:43 +0300

    Added the map first map logic
    Used cartoDB basemap. OSM caused visual issues

---
+++ map1/js/javascript.js
@@ -0,0 +1,9 @@
+let map = L.map('map').setView([58.373523, 26.716045], 12)
+
+const CartoDB_Voyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 20
+});
+
+CartoDB_Voyager.addTo(map)
